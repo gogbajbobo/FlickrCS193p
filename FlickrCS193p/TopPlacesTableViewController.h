d384fd44f028db1cdcc89d85332b057cbd653c24
Grigoriev Maxim <grigoriev.maxim@gmail.com>
2012-05-15 18:03:34 +0400

    add TableViewController for topPlaces

---
+++ FlickrCS193p/TopPlacesTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TopPlacesTableViewController.h
+//  FlickrCS193p
+//
+//  Created by Григорьев Максим on 5/15/12.
+//  Copyright (c) 2012 Maxim V. Grigoriev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TopPlacesTableViewController : UITableViewController
+
+@end
